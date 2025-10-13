08a171e966ed7bf36d76e68c240984ffed2e180a
U92014 <superunine@gmail.com>
2025-10-13 00:51:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.querySelectorAll(".toggle-password").forEach(btn => {
 
     if (input.type === "password") {
       input.type = "text";
-      btn.textContent = "🙈";
+      btn.textContent = "👁️";
     } else {
       input.type = "password";
       btn.textContent = "👁️";
