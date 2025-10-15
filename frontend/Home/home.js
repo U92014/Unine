e6e0d89cc54e9f7edda317c1a1e08e0d0d145fe4
U92014 <superunine@gmail.com>
2025-10-15 12:24:44 -0700

    Update home.js

--- frontend/Home/home.js
+++ frontend/Home/home.js
@@ -45,8 +45,8 @@ window.logout = async function () {
 
 const adUrls = [
   "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9shopping.jpg",
-  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9shopping1.jpg",
-  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9.png"
+  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9shopping.jpg",
+  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9shopping.jpg"
 ];
 
 let currentAdIndex = 0;
